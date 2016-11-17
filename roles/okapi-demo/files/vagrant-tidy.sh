bc5813934eb2c2db258d542ae2feb4ddc2133d86
David Crossley <dcrossley@indexgeo.com.au>
2016-11-17 15:48:55 +1100

    Remove trailing spaces

--- roles/okapi-demo/files/vagrant-tidy.sh
+++ roles/okapi-demo/files/vagrant-tidy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 cat - << EOWARNING
 WARNING: This script will fil up your left over disk space.
- 
+
 DO NOT RUN THIS WHEN YOUR VIRTUAL HD IS RAW!!!!!!
- 
+
 You should NOT do this on a running system.
 This is purely for making vagrant boxes damn small.
- 
+
 Press Ctrl+C within the next 10 seconds if you want to abort!!
- 
+
 EOWARNING
 sleep 10;
- 
+
 # Remove APT cache
 apt-get clean -y
 apt-get autoclean -y
@@ -29,17 +29,17 @@ rm -f /home/vagrant/.bash_history
 find /var/log -type f | while read f; do echo -ne '' > $f; done;
 
 # Whiteout root
-count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`; 
+count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`;
 let count--
 dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
 rm /tmp/whitespace;
- 
+
 # Whiteout /boot
 count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
 let count--
 dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
 rm /boot/whitespace;
- 
+
 swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
 swapoff $swappart;
 dd if=/dev/zero of=$swappart;
