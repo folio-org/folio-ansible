be0ccfe63b137b3ac231ee7b66c59d1c3183a438
Wayne Schneider <wayne@indexdata.com>
2017-06-30 12:44:09 -0500

    Update stripes-docker role to create build-module-descriptors.js file
    Required to support STRIPES-430 when not using the GitHub stripes-demo-platform.

---
+++ roles/stripes-docker/files/build-module-descriptors.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const childProcess = require('child_process');
+
+const argv0 = process.argv[1];
+const indir = 'node_modules/@folio';
+const outdir = 'ModuleDescriptors';
+
+console.log('* build-module-descriptors');
+if (!fs.existsSync(outdir)) {
+  fs.mkdirSync(outdir);
+}
+
+fs.readdir(indir, (err, filenames) => {
+  if (err) {
+    console.log(`${argv0}: cannot scan '${err.path}': ${err.message}`);
+    process.exit(1);
+  }
+
+  const sortedFilenames = filenames.sort();
+  for (let i = 0; i < sortedFilenames.length; i++) {
+    const filename = sortedFilenames[i];
+    if (filename.startsWith('stripes-')) continue;
+    console.log(`processing '${filename}'`);
+
+    const cmd = `node ${indir}/stripes-core/util/package2md.js ${indir}/${filename}/package.json > ${outdir}/${filename}.json`;
+    try {
+      const buffer = childProcess.execSync(cmd);
+      const output = buffer.toString();
+      if (output) console.log(`'${cmd}' produced unexpected output: '${output}'`);
+    } catch (exception) {
+      console.log(`${argv0}: cannot run '${cmd}':`, exception);
+      process.exit(2);
+    }
+  }
+});
