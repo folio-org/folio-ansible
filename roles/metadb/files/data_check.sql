779d97fc63845fbd47a4aae779dc6ddb37efd26e
John Malconian <malc@indexdata.com>
2023-10-11 13:03:25 -0400

    add conditional

--- roles/metadb/files/data_check.sql
+++
@@ -1,9 +0,0 @@
-DO LANGUAGE plpgsql $$
-    BEGIN
-        IF (SELECT COUNT(*) FROM pg_catalog.pg_namespace WHERE nspname LIKE 'folio\_%') > 0 THEN 
-            RAISE NOTICE 'DONE';
-        ELSE
-            RAISE NOTICE 'NOT DONE';
-        END IF;
-    END;
-$$;
